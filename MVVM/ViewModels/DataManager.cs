a9287da8b205291150abf4776d1583e3ce8aa055
student <student@K09-10>
2018-07-10 16:55:24 +0300

    1.0

---
+++ MVVM/ViewModels/DataManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+using MVVM.Models;
+using System.Collections.ObjectModel;
+
+namespace MVVM.ViewModels
+{
+    class DataManager : INotifyPropertyChanged
+    {
+        private XDocument doc;
+        private string path;
+
+        private Contact selectedContact;
+
+        public ObservableCollection<Contact> Contacts{ get; set; }
+
+        public Contact SelectedContact
+        {
+            get { return selectedContact; }
+            set
+            {
+                selectedContact = value;
+                OnPropertyChanged("SelectedContact");
+            }
+        }
+
+        public DataManager()
+        {
+            Contacts = new ObservableCollection<Contact>();
+            path = @"..\..\Data\Contacts.xml";
+            doc = XDocument.Load(path);
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            var res = doc.Element("root").Elements("contact").ToList();
+            foreach (var x in res)
+            {
+                Contact c = new Contact()
+                {
+                    Person = x.Attribute("person").Value,
+                    Phone = x.Attribute("phone").Value,
+                    Email = x.Attribute("email").Value
+                };
+                Contacts.Add(c);
+            }
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
--- MVVM/Views/MainWindow.xaml.cs
+++ MVVM/Views/MainWindow.xaml.cs
@@ -12,17 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MVVM.ViewModels;
 
-namespace MVVM
+namespace MVVM.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new DataManager();
         }
     }
 }
